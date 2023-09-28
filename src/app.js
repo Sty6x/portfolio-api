4b93189a6e45ef4cfa06b6c85a8a6409f6ab5dc6
francis <franzdiaz460@gmail.com>
2023-09-28 09:03:36 +0400

    creating admin credentials with jwt

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 7000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
 // all messages
 // create messages
@@ -10,15 +12,35 @@ const bodyParser = require("body-parser");
 
 // need to connect frontend and backend
 
+// every router requires a JWT except for the user post route
+console.log({ email: process.env.ADMIN, password: process.env.ADMIN_PASSWORD });
+jwt.sign(
+  { email: process.env.ADMIN, password: process.env.ADMIN_PASSWORD },
+  process.env.PRIVATE_KEY,
+  (err, token) => {
+    console.log(token);
+  }
+);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.get("/messages", (req, res) => {
+app.get("/messages", verifyToken, (req, res) => {
   res.json({
     statusCode: res.statusCode,
     message: "retrieves all of the messages ",
   });
 });
+
+function verifyToken(req, res, next) {
+  const headerBearer = req.headers["authorization"];
+  console.log(headerBearer);
+  if (headerBearer == null) {
+    res.send("Forbidden");
+  }
+  console.log("nice");
+  next();
+}
 
 // http method for frontend
 app.post("/create-message", (req, res) => {
